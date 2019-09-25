6491851b1e5d9cb770c6c10bf089fb9f29450d91
Joshua Jones <joshuajones@trentu.ca>
2019-09-25 14:48:42 +0700

    Yes and No buttons added, Progress bar added

--- src/components/Topic.js
+++ src/components/Topic.js
@@ -2,6 +2,7 @@ import React from 'react'
 import '../styles/topic.css'
 import Comment from './Comment'
 import Nav from './Nav'
+import { ProgressBar } from 'react-rainbow-components'
 
 class Topic extends React.Component {
 
@@ -27,6 +28,18 @@ class Topic extends React.Component {
 					<h1>Title</h1>
 					<img src="https://images2.minutemediacdn.com/image/upload/c_fill,g_auto,h_1248,w_2220/f_auto,q_auto,w_1100/v1555924300/shape/mentalfloss/185187503.jpg" alt="landscape with rainbows and colorful birds"/>
 					<div>Description:</div>
+				</div>
+				<br></br>
+				<br></br>
+				<br></br>
+				<br></br>
+
+				<div className="bar">
+					<button className="yesButton">Yes</button>
+
+						<ProgressBar value={10} size="large" className="barImage"/>
+
+					<button className="noButton">No</button>
 				</div>
 				<br></br>
 				<br></br>
