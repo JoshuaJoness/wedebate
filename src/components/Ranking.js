21e0bc203839e1c35da1d6162efc0f7864b35f91
Nil <nilarmengol@gmail.com>
2019-10-02 10:12:31 +0700

    changed ranking Table

--- src/components/Ranking.js
+++ src/components/Ranking.js
@@ -1,45 +1,89 @@
+import DataTable from 'react-data-table-component';
 import React from 'react';
-import SortTable from './SortTable'
-import Nav from './Nav'
+import { Table, Column, StatusBadge, GlobalHeader, ButtonGroup, ButtonIcon, FontAwesomeIcon } from 'react-rainbow-components';
 import axios from 'axios'
 
+class SortTable extends React.Component {
 
+	state= {
+		data: [
+			{
+				ranking: 0,
+					username: 'Leandro Torres',
+					score: 0,
+					rank: 'sophist',
+					topics: 0,
+					opinions: 0,
+					comments: 0
 
-class Ranking extends React.Component {
+			},
 
-
-
-	state = {
-		rankings: [{
-        avatar: "",
-        email: "",
-        username: "",
-				score: 0,
-        topics: 0,
-        opinions: 0,
-        comments: 0
-    },]
-	}
+		],
+		columns: [  {
+		    name: 'Ranking',
+		    selector: 'ranking',
+		    sortable: true,
+		  },
+		  {
+		    name: 'Username',
+		    selector: 'username',
+		    sortable: true,
+		    right: true,
+		  },
+			{
+				name: 'Score',
+				selector: 'score',
+				sortable: true,
+				right: true,
+			},
+			{
+				name: 'Rank',
+				selector: 'rank',
+				sortable: true,
+				right: true,
+			},
+			{
+				name: 'Topics',
+				selector: 'topics',
+				sortable: true,
+				right: true,
+			},
+			{
+				name: 'Opinions',
+				selector: 'opinions',
+				sortable: true,
+				right: true,
+			},
+			{
+				name: 'Comments',
+				selector: 'comments',
+				sortable: true,
+				right: true,
+			},
+		]}
 
 		componentWillMount() {
 			axios.get('http://localhost:4000/rankings')
 			  .then(res => {
 			    this.setState({
-			      rankings: res.data
+			      data: res.data
 			    })
 			  })
 			  .catch(err => {})
 		}
 
-	render() {
 
-		return(
-						<>
-							<Nav/>
-							<h1 style={{textAlign: 'center'}}>Ranking</h1>
-							<SortTable/>
-						</>
-		)
-	}
+
+  render() {
+    return (
+      <DataTable
+        title="Ranking"
+        columns={this.state.columns}
+        data={this.state.data}
+      />
+    )
+  }
 }
-export default Ranking
+
+
+export default SortTable
--- src/components/SortTable.js
+++ src/components/SortTable.js
@@ -1,90 +1,131 @@
+
 import React from 'react';
-import { Table, Column, StatusBadge, GlobalHeader, ButtonGroup, ButtonIcon, FontAwesomeIcon } from 'react-rainbow-components';
+import { Table, Column, StatusBadge, GlobalHeader, ButtonGroup, ButtonIcon } from 'react-rainbow-components';
 import axios from 'axios'
 
 
+
 class SortTable extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-				sortedBy: 'score',
-				sortDirection: 'asc',
-				data: [
-						{
-							ranking: 1,
-								username: 'Leandro Torres',
-								score: 0,
-								rank: 'sophist',
-								topics: 0,
-								opinions: 0,
-								comments: 0
 
-						},
+    constructor(props) {
+        super(props);
+        this.state = {
+            sortedBy: undefined,
+            sortDirection: 'asc',
+            data: [
+                {
+                    name: 'Leandro Torres',
+                    company: 'Nexxtway',
+                    email: 'leandro@gmail.com',
+                    status: 'verified',
+                    id: '1234qwerty',
+                },
+                {
+                    name: 'Reinier',
+                    company: 'Nexxtway',
+                    email: 'reinier@gmail.com',
+                    status: 'verified',
+                    id: '1234asdfgh',
+                },
+                {
+                    name: 'Jose Torres',
+                    company: 'Google',
+                    email: 'jose@gmail.com',
+                    status: 'verified',
+                    id: '1234zxcvbn',
+                },
+                {
+                    name: 'Sara',
+                    company: 'Nexxtway',
+                    email: 'sara@gmail.com',
+                    status: 'verified',
+                    id: '5678qwerty',
+                },
+                {
+                    name: 'Tahimi',
+                    company: 'Nexxtway',
+                    email: 'tahimi@gmail.com',
+                    status: 'verified',
+                    id: '5678asdfgh',
+                },
+                {
+                    name: 'Alejandro',
+                    company: 'Google',
+                    email: 'alejandro@gmail.com',
+                    status: 'verified',
+                    id: '5678zxcvbn',
+                },
+                {
+                    name: 'Carlos',
+                    company: 'Oracle',
+                    email: 'carlos@gmail.com',
+                    status: 'verified',
+                    id: '9012qwerty',
+                },
+                {
+                    name: 'Luis',
+                    company: 'Google',
+                    email: 'luis@gmail.com',
+                    status: 'verified',
+                    id: '9012asdfgh',
+                },
+            ],
+        };
+        this.handleOnSort = this.handleOnSort.bind(this);
+    }
 
-				],
-		};
-		this.handleOnSort = this.handleOnSort.bind(this);
+    handleOnSort(event, field, nextSortDirection) {
+        const { data, sortedBy, sortDirection } = this.state;
+
+        let newData = [...data];
+
+        const key = x => x[field];
+        const reverse = nextSortDirection === 'asc' ? 1 : -1;
+
+        const sortedData = newData.sort((a, b) => {
+            a = key(a);
+            b = key(b);
+            return reverse * ((a > b) - (b > a));
+        });
+
+        this.setState({ data: sortedData, sortedBy: field, sortDirection: nextSortDirection });
+    }
+
+    render() {
+        const { data, sortDirection, sortedBy } = this.state;
+        return (
+            <div className="rainbow-p-bottom_xx-large">
+                <GlobalHeader className="rainbow-m-bottom_xx-large" src="images/user/user3.jpg">
+                    <ButtonGroup className="rainbow-m-right_medium">
+                        <ButtonIcon
+                            variant="border-filled"
+                            disabled
+
+                        />
+                        <ButtonIcon
+                            variant="border-filled"
+                            disabled
+
+                        />
+                    </ButtonGroup>
+                </GlobalHeader>
+
+                    <Table
+                        keyField="id"
+                        data={data}
+                        onSort={this.handleOnSort}
+                        sortDirection={sortDirection}
+                        sortedBy={sortedBy}
+                    >
+                        <Column header="Name" field="name" sortable />
+                        <Column header="Status" field="status" component={StatusBadge} />
+                        <Column header="Company" field="company" />
+                        <Column header="Email" field="email" sortable />
+                    </Table>
+                </div>
+
+        );
+    }
 }
 
-
-
-
-
-		componentWillMount() {
-			axios.get('http://localhost:4000/rankings')
-			  .then(res => {
-			    this.setState({
-			      data: res.data
-			    })
-			  })
-			  .catch(err => {})
-		}
-
-
-handleOnSort(event, field, nextSortDirection) {
-		const { data, sortedBy, sortDirection } = this.state;
-
-		let newData = [...data];
-
-		const key = x => x[field];
-		const reverse = nextSortDirection === 'asc' ? 1 : -1;
-
-		const sortedData = newData.sort((a, b) => {
-				a = key(a);
-				b = key(b);
-				return reverse * ((a > b) - (b > a));
-		});
-
-		this.setState({ data: sortedData, sortedBy: field, sortDirection: nextSortDirection });
-}
-
-	render() {
-
-		return(
-
-
-			<div className="rainbow-p-bottom_xx-large">
-				<div style={this.tableContainerStyles}>
-	                    <Table
-	                        keyField="id"
-	                        data={this.state.data}
-	                        onSort={this.handleOnSort}
-	                        sortDirection={this.sortDirection}
-	                        sortedBy={this.sortedBy}
-	                    >
-
-												<Column header="ranking" field="ranking" sortable />
-												<Column header="username" field="username" sortable />
-                        <Column header="rank" field="rank" component={StatusBadge} />
-												<Column header="score" field="score" sortable />
-                        <Column header="topics" field="topics" />
-                        <Column header="opinions" field="opinions" sortable />
-												 <Column header="comments" field="comments" sortable />
-		     							</Table>
-				 						</div>
-									</div>
-
-		)
-	}
-}
 export default SortTable
