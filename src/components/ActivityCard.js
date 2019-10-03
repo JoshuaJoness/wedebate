64da4741bf2b9936a3e564c6ac157d08ec242bf7
Nil <nilarmengol@gmail.com>
2019-10-03 17:05:27 +0700

    added token to HomePage

--- src/components/ActivityCard.js
+++ src/components/ActivityCard.js
@@ -3,6 +3,7 @@ import {Card, Button, ButtonIcon} from "react-rainbow-components";
 import Circle from "./Circle";
 import "../styles/profile.css";
 
+
 class ActivityCard extends React.Component {
  	render() {
    return (
@@ -19,7 +20,7 @@ class ActivityCard extends React.Component {
          <label>{this.props.user.username} - need to import opinion creation date and then format</label>
          <p>
 					 {this.props.opinion}
-           
+
          </p>
        </div>
 
