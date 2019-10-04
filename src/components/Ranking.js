7cf9bbef44f33cc1e05425f2be3d946eea34bea6
Nil <nilarmengol@gmail.com>
2019-10-04 11:07:49 +0700

    optimized Ranking page

--- src/components/Ranking.js
+++ src/components/Ranking.js
@@ -71,30 +71,10 @@ class SortTable extends React.Component {
 
 			let token = localStorage.getItem('token')
 
-			axios.get('http://localhost:4000/profile', {
-				headers: {
-					Authorization: `Bearer ${token}`
-				}
-			}).then(res => {
-				let user = this.state.user
-				user = res.data
-				this.setState({user})
-			}).catch(err => {
-				console.log(err);
-			})
 
 
-			axios.get('http://localhost:4000/ranking', {
-				headers: {
-					Authorization: `Bearer ${token}`
-				}
-			}).then(res => {
-				let points = this.state.points
-				points = res.data.total
-				this.setState({points})
-			}).catch(err =>{
-				console.log(err)
-			})
+
+	
 
 			axios.get('http://localhost:4000/rankings')
 			  .then(res => {
