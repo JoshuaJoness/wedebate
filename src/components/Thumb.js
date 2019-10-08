a552188aec47da564d697bfbe800bf41dfc934c5
Nil <nilarmengol@gmail.com>
2019-10-08 11:24:14 +0700

    thumbnail css until circle

--- src/components/Thumb.js
+++ src/components/Thumb.js
@@ -18,24 +18,27 @@ class Thumb extends React.Component {
 
 	render() {
     return (
-      <div className="card link" href="place.html">
-			<img
-						src={this.props.topic.image}
-						alt="landscape with rainbows and colorful birds"
-				/>
-        <div className="content">
+      <div className="cardWrap" href="place.html">
+			<div
+				className="imageHome"
+				style={{
+					backgroundImage: `url(${this.props.topic.image})`
+				}}
+			></div>
+        <div className="titleHome">
           <h1 className="meta">
             {this.props.topic.title}
           </h1>
+					</div>
           <small className="location">
- <div className='yesno'>
-                     <Circle/>
- </div>
+					<div className='yesno'>
+															<Circle topic={this.props.topic}/>
+					</div>
           </small >
           <div> > holaaa</div>
                        <span className="date"> 2 days ago</span>
         </div>
-      </div>
+
     );
   }
  }
