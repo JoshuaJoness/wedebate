1679c005e6a484373e0d8aafa57f4a5a1e9d3230
Nil <nilarmengol@gmail.com>
2019-10-09 16:28:34 +0700

    send image instead of avatar to API

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,188 +1,196 @@
-import React from 'react'
-import { CheckboxGroup, Button } from 'react-rainbow-components'
-import { Input } from 'react-rainbow-components'
-import {Link} from 'react-router-dom'
-import axios from 'axios'
-import '../styles/forms.css'
-
+import React from "react";
+import { CheckboxGroup, Button } from "react-rainbow-components";
+import { Input } from "react-rainbow-components";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import "../styles/forms.css";
 
 const inputContainerStyles = {
-    width: '50%',
-}
+  width: "50%"
+};
 
 const options = [
-    {
-			value: 'checkboxOne',
-			label: "By checking this box you hereby grant 'We Debate' to use your image in any way that they see fit within the context of the site. You also certify that I have full legal rights to this image.",
-			disabled: false
-		},
-		{
-			value: 'checkboxTwo',
-			label: 'By checking this box I certify that I am of legal age in my jurisdiction to use this site.',
-			disabled: false
-		}
-]
+  {
+    value: "checkboxOne",
+    label:
+      "By checking this box you hereby grant 'We Debate' to use your image in any way that they see fit within the context of the site. You also certify that I have full legal rights to this image.",
+    disabled: false
+  },
+  {
+    value: "checkboxTwo",
+    label:
+      "By checking this box I certify that I am of legal age in my jurisdiction to use this site.",
+    disabled: false
+  }
+];
 
 class Signup extends React.Component {
+  state = {
+    user: {
+      avatar: "",
+      email: "",
+      username: "",
+      password: ""
+    },
+    values: ["false", "false"]
+  };
+  //
+  // constructor(props) {
+  // 		super(props);
+  // 		this.state = { values: ['', ''] };
+  // 		this.handleOnChange = this.handleOnChange.bind(this);
+  // }
 
-	state= {
-		user: {
-			avatar:'',
-			email:'',
-			username:'',
-			password:'',
-		},
-		values: ['false','false']
-	}
-	//
-	// constructor(props) {
-	// 		super(props);
-	// 		this.state = { values: ['', ''] };
-	// 		this.handleOnChange = this.handleOnChange.bind(this);
-	// }
+  handleOnChange = values => {
+    this.setState({ values });
+  };
 
+  changeField = (e, field) => {
+    let user = this.state.user;
+    user[field] = e.target.value;
+    this.setState({ user });
+    console.log(this.state.user);
+  };
 
+  getFile = e => {
+    let file = e.target.files[0];
+    let user = this.state.user;
+    user.avatar = file;
+    this.setState({
+      user: user
+    });
+    console.log(file);
+  };
 
+  submit = e => {
+    e.preventDefault();
+    let user = this.state.user;
+    console.log("this.state", this.state);
+    let data = new FormData();
 
-	handleOnChange = (values) => {
-			this.setState({ values });
-	}
+    data.append("image", this.state.user.avatar);
+    data.append("email", this.state.user.email);
+    data.append("username", this.state.user.username);
+    data.append("password", this.state.user.password);
 
-	changeField = (e, field) => {
-		let user = this.state.user
-		user[field] = e.target.value
-		this.setState({user})
-		console.log(this.state.user);
-	}
+    console.log(data);
 
-	getFile = (e) => {
-	  let file = e.target.files[0]
-		let user = this.state.user
-		user.avatar = file
-		this.setState({
-		user: user
-		})
-console.log(file)
-	}
+    if (
+      user.email &&
+      user.username &&
+      user.password &&
+      this.state.values.includes("checkboxOne" && "checkboxTwo")
+    ) {
+      axios
+        .post(`${process.env.REACT_APP_API}/signup`, data)
+        .then(res => {
+          localStorage.setItem("token", res.data.token);
+          console.log(res.data);
+          this.props.history.push("/");
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    } else {
+      alert("Please complete all fields, and check all boxes.");
+    }
+  };
 
+  render() {
+    const styles = {
+      button: {
+        color: "violet",
+        borderRadius: "10px",
+        height: "40px",
+        background: "#DCDCDC",
+        border: "0.5px solid silver",
+        marginBottom: "10px"
+      },
+      link: {
+        fontSize: "20px",
+        fontFamily: "'Roboto Mono', monospace",
+        fontWeight: "bolder",
+        textAlign: "center",
+        marginLeft: "190px"
+      }
+    };
 
-	submit = (e) => {
-		e.preventDefault()
-		let user = this.state.user
-		console.log('this.state', this.state);
-		let data = new FormData()
-
-		data.append('avatar', this.state.user.avatar)
-		data.append('email', this.state.user.email)
-		data.append('username', this.state.user.username)
-		data.append('password', this.state.user.password)
-
-		console.log(data)
-
-
-
-		if(user.email && user.username && user.password && this.state.values.includes('checkboxOne' && 'checkboxTwo'))
-		{
-		axios.post(`${process.env.REACT_APP_API}/signup`,
-		 data).then(res =>{
-			localStorage.setItem('token', res.data.token)
-			console.log(res.data)
-			this.props.history.push("/")
-		}).catch(err =>{
-			console.log(err);
-		})
-		} else {
-		alert('Please complete all fields, and check all boxes.')
-			}
-		}
-
-
-
-	render() {
-
-		const styles = {
-			button: {
-				color: 'violet',
-				borderRadius: '10px',
-				height: '40px',
-				background: '#DCDCDC',
-				border: '0.5px solid silver',
-				marginBottom: '10px'
-			},
-			link:{
-				fontSize: '20px',
-				fontFamily: "'Roboto Mono', monospace",
-				fontWeight: 'bolder',
-				textAlign: 'center',
-				marginLeft: '190px'
-			}
-		}
-
-
-		return(
-			<>
-					<div class="grid center-large small image">
-
-<div>
-
-
-					<form className="formSignup" onSubmit={this.submit}>
-						<Link to="/" style={styles.link}>We Debate</Link>
-						<span className="title">Sign Up</span>
-						<div className="rainbow-p-vertical_large rainbow-p-horizontal_xx-large rainbow-m-horizontal_xx-large">
-						  <Input
-						   	className="formInput"
-						   	label="Please enter your email:"
-						   	placeholder="example@email.com"
-								type="email"
-								onChange={(e)=>this.changeField(e,'email')} />
-							<Input
-								className="formInput"
-								label="Please enter a username:"
-								placeholder="username"
-								type="text"
-								onChange={(e)=>this.changeField(e,'username')} />
-							<Input
-								className="formInput"
-								label="Please enter a password:"
-								placeholder="password"
-								type="password"
-								onChange={(e)=>this.changeField(e,'password')} />
-							<div className="uploadLabel">Please select a profile picture:</div>
-							<div className="rainbow-p-vertical_large rainbow-p-left_xx-large" className="upload">
-								<br></br>
-								<input type="file"name="myFile"onChange={this.getFile} />
-								<br></br>
-								<br></br>
-							</div>
-							<div className="rainbow-p-vertical_large rainbow-p-left_xx-large " className="checkbox">
-								<CheckboxGroup
-									style={styles.checkbox}
-									label=""
-									required='true'
-									options={options}
-									value={this.state.values}
-									onChange={this.handleOnChange}
-
-								/>
-							</div>
-							<div className="rainbow-p-vertical_large rainbow-p-left_xx-large">
-										<button style={styles.button}  className="primary signupButton ">Sign Up</button>
-							</div>
-							<div className="rainbow-p-vertical_large rainbow-p-left_xx-large">
-								<p className="footerSignup">
-									Already have an account? <Link to="/Login">Login</Link>
-								</p>
-							</div>
-						</div>
-					</form>
-
-</div>
-
-				</div>
-			</>
-		)
-	}
+    return (
+      <>
+        <div class="grid center-large small image">
+          <div>
+            <form className="formSignup" onSubmit={this.submit}>
+              <Link to="/" style={styles.link}>
+                We Debate
+              </Link>
+              <span className="title">Sign Up</span>
+              <div className="rainbow-p-vertical_large rainbow-p-horizontal_xx-large rainbow-m-horizontal_xx-large">
+                <Input
+                  className="formInput"
+                  label="Please enter your email:"
+                  placeholder="example@email.com"
+                  type="email"
+                  onChange={e => this.changeField(e, "email")}
+                />
+                <Input
+                  className="formInput"
+                  label="Please enter a username:"
+                  placeholder="username"
+                  type="text"
+                  onChange={e => this.changeField(e, "username")}
+                />
+                <Input
+                  className="formInput"
+                  label="Please enter a password:"
+                  placeholder="password"
+                  type="password"
+                  onChange={e => this.changeField(e, "password")}
+                />
+                <div className="uploadLabel">
+                  Please select a profile picture:
+                </div>
+                <div
+                  className="rainbow-p-vertical_large rainbow-p-left_xx-large"
+                  className="upload"
+                >
+                  <br></br>
+                  <input type="file" name="myFile" onChange={this.getFile} />
+                  <br></br>
+                  <br></br>
+                </div>
+                <div
+                  className="rainbow-p-vertical_large rainbow-p-left_xx-large "
+                  className="checkbox"
+                >
+                  <CheckboxGroup
+                    style={styles.checkbox}
+                    label=""
+                    required="true"
+                    options={options}
+                    value={this.state.values}
+                    onChange={this.handleOnChange}
+                  />
+                </div>
+                <div className="rainbow-p-vertical_large rainbow-p-left_xx-large">
+                  <button
+                    style={styles.button}
+                    className="primary signupButton "
+                  >
+                    Sign Up
+                  </button>
+                </div>
+                <div className="rainbow-p-vertical_large rainbow-p-left_xx-large">
+                  <p className="footerSignup">
+                    Already have an account? <Link to="/Login">Login</Link>
+                  </p>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </>
+    );
+  }
 }
 
-export default Signup
+export default Signup;
